Fixed sign statistic and incomplete draft of error type II

main.py
@@ -39,22 +39,20 @@ def calculateErrorTypeI( scenarios, data, index, rejectAlpha):
     return results
 
 def calculateErrorTypeII( scenarios, data, index, rejectAlpha, errorTypeIIParameters):
-    results = []
-    for numberOfEvents in scenarios:
-        statistics = runStudy(data, index, numberOfEvents, errorTypeIIParameters)
-        #This needs to be fixed because statistics is now going to be multidimensional
-        errorsTypeII = [ Statistics.errorTypeII(statistic, rejectAlpha) for statistic in statistics ]
-        results.append(errorsTypeII)
+     results = []
+     for numberOfEvents in scenarios:
+         statistics = runStudy(data, index, numberOfEvents, errorTypeIIParameters)
+         #This needs to be fixed because statistics is now going to be multidimensional
+         errorsTypeII = [ Statistics.errorTypeII(statistic, rejectAlpha) for statistic in statistics ]
+         results.append(errorsTypeII)
 
-    results = np.array(results).T.tolist()
-    return results
-
-############################################################
+     results = np.array(results).T.tolist()
+     return results
 
 data, index = loadVariables()
 
 
-scenarios = [20,30,50,80,100,130,150,200]
+scenarios = [20,30,50,80,100]#,130,150,200]
 rejectAlpha = 0.05
 errorTypeIIParameters = {'lambdas':[ 0.1, 1, 10 ], 'etas':[ 0.5, 1, 2 ]}
 
@@ -66,7 +64,7 @@ for i, error in enumerate(results):
 plt.legend()
 plt.show()
 
-results = calculateErrorTypeII( [100], data, index, rejectAlpha, errorTypeIIParameters )
+# results = calculateErrorTypeII( [100], data, index, rejectAlpha, errorTypeIIParameters )
 
 
 

statistics.py
@@ -47,12 +47,10 @@ class Statistics( object ):
         return cummulativeRankInEventWindow/S/np.sqrt(self.eventWindowSize)
 
     def Sign_statistic(self):
-        abnormalReturns = np.concatenate((self.estimationAbnormalReturns, self.eventAbnormalReturns), axis=2)
-        signMatrix = np.sign( abnormalReturns )
-        absAbnormalReturns = np.abs( abnormalReturns )
-        rankMatrix = np.argsort(absAbnormalReturns, axis=2)
-        signedRankMatrix = rankMatrix*signMatrix
-        return np.sum( signedRankMatrix, axis=2 )/np.sqrt( np.sum( np.power( signedRankMatrix, 2), axis=2 ) )
+        signMatrix = np.sign( self.eventAbnormalReturns )
+        positiveCount = np.sum( np.count_nonzero( signMatrix == 1,axis=1), axis=1 )
+        N = signMatrix.size/signMatrix.shape[0]
+        return (positiveCount/N - 0.5)*np.sqrt(N)/0.5
 
 
 
@@ -75,6 +73,15 @@ class Statistics( object ):
 
     @staticmethod
     def errorTypeI( statistic, alpha):
+        sigma = np.std( statistic )
+        zAlphaBillateral = scipy.stats.norm.ppf( 1-alpha/2 )
+        thresholdForRejection = sigma * zAlphaBillateral
+        occurences = np.where( np.abs(statistic)>thresholdForRejection )[0]
+
+        return len(occurences)/len(statistic)
+
+    @staticmethod
+    def errorTypeII( statistic, alpha):
         sigma = np.std( statistic )
         zAlphaBillateral = scipy.stats.norm.ppf( 1-alpha/2 )
         thresholdForRejection = sigma * zAlphaBillateral